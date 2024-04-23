c130928fc5609c5aa98886cec224c9b109f22378
Snehal_Salunke1911 <snehalnssalunke@gmail.com>
2024-04-23 20:54:30 +0530

    hover effect on project card and adding services

--- client/src/components/Services.js
+++ client/src/components/Services.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { faComment, faTools, faBuilding, faPaintBrush, faLandmark } from '@fortawesome/free-solid-svg-icons'; // Importing different icons
+import { faComment, faTools, faMoneyBill, faLevelUp,faBuilding, faPaintBrush, faLandmark, } from '@fortawesome/free-solid-svg-icons'; // Importing different icons
 
 import ServiceCard from './ServiceCard';
 
@@ -17,6 +17,8 @@ function Services() {
           <ServiceCard icon={faComment} service="Construction Consultancy" description="Expert guidance for successful construction projects. Our consultancy service ensures strategic planning and efficient project management, maximizing outcomes and minimizing risks."/>
           <ServiceCard icon={faTools} service="Planning" description="Strategic blueprints for seamless construction projects. Our planning service provides meticulous analysis and detailed roadmaps, ensuring projects are executed with precision and efficiency."/>
           <ServiceCard icon={faBuilding} service="RCC Design & Supervision" description="Structural integrity ensured with precision. Our RCC design and supervision service offers meticulous oversight, ensuring robust structures and adherence to industry standards."/>
+          <ServiceCard icon={faMoneyBill} service="Estimation & Valuation" description="Structural integrity ensured with precision. Our RCC design and supervision service offers meticulous oversight, ensuring robust structures and adherence to industry standards."/>
+          <ServiceCard icon={faLevelUp} service="3D Elevations" description="Structural integrity ensured with precision. Our RCC design and supervision service offers meticulous oversight, ensuring robust structures and adherence to industry standards."/>
           <ServiceCard icon={faPaintBrush} service="Exterior and Interior Designing" description="Elevating spaces with aesthetic brilliance. Our exterior and interior design service transforms environments, blending creativity with functionality to create captivating and harmonious living and working spaces."/>
           <ServiceCard icon={faLandmark} service="N.A. Layouts" description="Unlocking potential through thoughtful layouts. Our N.A. layouts service crafts strategic plans tailored to optimize land use, fostering sustainable and harmonious communities."/>
           {/* Add more ServiceCards with different icons as needed */}
--- client/src/pages/Admin/Product.js
+++ client/src/pages/Admin/Product.js
@@ -5,17 +5,49 @@ import { Link } from "react-router-dom";
 import AdminMenu from "../../components/AdminMenu";
 import Layout from "../../components/Layout";
 
+const MyComponent = ({ src }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  return (
+    <div
+      style={{
+        width: '100%',
+        height: '100%',
+        transition: 'transform 0.3s',
+        transform: isHovered ? 'scale(1.1)' : 'scale(1)',
+      }}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <img
+        src={src}
+        className="card-img-top"
+        alt=""
+        style={{ width: '100%', height: '100%' }}
+      />
+    </div>
+  );
+};
+
 const Products = () => {
   const [products, setProducts] = useState([]);
 
-  //getall products
+  //get all products
   const getAllProducts = async () => {
     try {
       const { data } = await axios.get(`/api/v1/product/get-product`);
       setProducts(data.products);
     } catch (error) {
       console.log(error);
-      toast.error("Someething Went Wrong");
+      toast.error("Something Went Wrong");
     }
   };
 
@@ -23,32 +55,43 @@ const Products = () => {
   useEffect(() => {
     getAllProducts();
   }, []);
+
   return (
     // <Layout>
-      <div className="row">
-        <div className="col-md-12 ">
-        <div className="section-header section-bg" style={{padding:"50px"}}><h2 style={{paddingTop:"20px"}}>Portfolio</h2></div>       
-          <div className="d-flex flex-wrap">
-            {products?.map((p) => (
-             
-                <div className="card m-1 mx-auto" style={{ width: '400px', height: 'mh-100' }} >
-                  <img
-                    src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${p._id}`}
-                    className="card-img-top"
-                    alt={p.name}
-                  />
-                  <div className="card-body"> 
-                    <h5 className="card-title">{p.name}</h5>
-                    <p className="card-text"><span className="font-weight-bold"> </span> {p.description} </p>
-                    <p className="card-text"><span className="font-weight-bold"> </span> {p.price} </p>
-                    <p className="card-text">{p.quantity}</p>
-                  </div> 
-                </div>
-              
-            ))}
-          </div>
+    <div className="row">
+      <div className="col-md-12 ">
+        <div className="section-header section-bg" style={{ padding: "50px" }}>
+          <h2 style={{ paddingTop: "20px" }}>Portfolio</h2>
+        </div>
+        <div className="d-flex flex-wrap">
+          {products?.map((p) => (
+            <div
+              key={p._id}
+              className="card m-1 mx-auto"
+              style={{
+                width: '400px',
+                height: 'mh-100',
+                transition: 'transform 0.3s ease-in-out',
+              }}
+            >
+              <MyComponent
+                src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${p._id}`}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{p.name}</h5>
+                <p className="card-text">
+                  <span className="font-weight-bold"> </span> {p.description}{" "}
+                </p>
+                <p className="card-text">
+                  <span className="font-weight-bold"> </span> {p.price}{" "}
+                </p>
+                <p className="card-text">{p.quantity}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
+    </div>
     // </Layout>
   );
 };
